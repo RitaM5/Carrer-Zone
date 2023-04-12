67059c3a10e3b34397bf13c3908d5a9b7eb24226
RitaM5 <sporshia108514orva@gmail.com>
2023-04-12 23:45:41 +0600

    project done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
             <Banner />
             <section className='my-container mt-6'>
                 <p className='text-4xl font-semibold text-center'>Job Category List</p>
-                <p className='text-center text-sm my-8 w-96 mx-auto'>
+                <p className='text-center text-sm my-8 w-full'>
                 By selecting the category or job type that matches your skills and experience, you can more easily find job opportunities that are relevant to your career goals.
                 </p>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-12'>
@@ -39,7 +39,7 @@ const Home = () => {
             </section>
             <section className='my-16 my-container'>
                 <p className='text-4xl font-semibold text-center'>Featured Jobs</p>
-                <p className='text-center text-sm my-8 w-96 mx-auto'>
+                <p className='text-center text-sm my-8'>
                 These features can help you to find and apply for job opportunities more efficiently and increase your chances of landing your desired job
                 </p>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mt-16'>
